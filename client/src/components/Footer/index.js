8d5377b0d249c167b26ec8e1698f094340d63e0a
Emma Olson <emma.olson794@gmail.com>
2022-08-02 13:58:24 -0500

    write basic footer component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,21 @@
 // imports
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // import font awesome componenet
+import { faGithub } from '@fortawesome/free-brands-svg-icons'; // import selected icons
 
 
 function Footer() {
     return (
-        <div></div>
+        <footer className='footer'>
+            <h3> &copy; A Dish A Day 2022</h3>
+            <ul className='footer-list'>
+                <li>
+                    <a href="https://github.com/MNlegion/A-Dish-A-Day">
+                    <FontAwesomeIcon icon={ faGithub } size="lg" />
+                    </a>
+                </li>
+            </ul>
+        </footer>
     )
 }
 
