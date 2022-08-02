8593f9353f4c7b5f904a1ee6f8318d36e0090fed
Emma Olson <emma.olson794@gmail.com>
2022-08-02 17:28:40 -0500

    change landing from container to section tag

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -5,13 +5,13 @@ import Header from '../../components/Header';
 function Landing() {
 
     return(
-        <Container className='landing-container'>
+        <section className='landing-container'>
             <Paper className='landing-hero' elevation={3}>
                 <Header></Header>
                 <h1>for independent recipe writers <br /> & home chefs</h1>
                 <Button variant='contained' href=''>Login or Signup</Button>
             </Paper>
-        </Container>
+        </section>
 
     )
 };
