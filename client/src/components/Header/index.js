f9fdc68a4e06f717bd2723d1b085ff695645e6fa
MNlegion <rkreuser30@gmail.com>
2022-08-02 19:45:24 -0500

    navigation component setup and test import on header component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navigation from '../Navigation';
 
 
 function Header() {
@@ -6,6 +7,7 @@ function Header() {
         <div>
             <h1>A Dish A Day</h1>
             <p>Nav component coming soon</p>
+            <Navigation></Navigation>
         </div>
     )
 }
--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,11 +1,45 @@
 // imports
 
 
-function Navigation() {
-    return (
-        <div></div>
-    )
-}
+// function Navigation() {
+//     return (
+//         <div></div>
+//     )
+// }
 
+
+// export default Navigation;
+
+import React from "react";
+
+function Navigation(props) {
+  const tabs = ["Login", "Protein Search", "View All Recipes", "About"];
+  return (
+    <div>
+      <ul>
+        {tabs.map((tab) => (
+          <li
+            className={
+              props.currentPage === tab ? "nav-item is-active" : "nav-item"
+            }
+            key={tab}
+          >
+            <a
+              href={"#" + tab.toLowerCase()}
+              // Whenever a tab is clicked on,
+              // the current page is set through the handlePageChange props.
+              onClick={() => props.handlePageChange(tab)}
+              className={
+                props.currentPage === tab ? "nav-link active" : "nav-link"
+              }
+            >
+              {tab}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default Navigation;
