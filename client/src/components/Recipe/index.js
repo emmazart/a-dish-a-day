a36efcfea6a0fe39861b40c186d5fb3ef0614d82
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-08-03 18:00:05 -0400

    added rating component and added to pages

--- client/src/components/Recipe/index.js
+++ client/src/components/Recipe/index.js
@@ -15,8 +15,8 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 // import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
+import Rating from '../Rating';
 
 // interface ExpandMoreProps extends IconButtonProps {
 //   expand: boolean;
@@ -62,9 +62,7 @@ export default function RecipeCard({recipe}) {
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+          <Description/>
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
