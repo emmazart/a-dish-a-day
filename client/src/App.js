ef5ee5aa397ce50effd84e72cb2b76672a4f5274
Emma Olson <emma.olson794@gmail.com>
2022-08-02 19:37:50 -0500

    basic router working

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import { UserProvider } from './utils/GlobalState';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from '../src/pages/Landing';
 import SignInOrSignUp from '../src/pages/SignInOrSignUp';
 
 function App() {
   return (
     <UserProvider>
-      {/* <Landing className="landing-page"></Landing> */}
-      <SignInOrSignUp></SignInOrSignUp>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<Landing className="landing-page" />}
+          />
+          <Route 
+            path="/login"
+            element={<SignInOrSignUp></SignInOrSignUp>}
+          />
+        </Routes>
+      </Router>
     </UserProvider>
   );
 }
--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -4,12 +4,23 @@ import Header from '../../components/Header';
 
 function Landing() {
 
+    function handleLogin() {
+        // route to SignInOrSignUp page
+        // render Login form
+    }
+
+    function handleSignup() {
+        // route to SignInOrSignUp page
+        // render Sign up form
+    }
+
     return(
         <section className='landing-container'>
             <Paper className='landing-hero' elevation={3}>
                 <Header></Header>
                 <h1>for independent recipe writers <br /> & home chefs</h1>
-                <Button variant='contained' href=''>Login or Signup</Button>
+                <Button variant='contained' onClick={handleLogin}>Login</Button>
+                <p onClick={handleSignup}><a>Don't have an account? Sign Up</a></p>
             </Paper>
         </section>
 
