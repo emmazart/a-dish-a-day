bc1c5ec3cfa9affd9c346b2ef24f2f1f3b8e453b
MNlegion <rkreuser30@gmail.com>
2022-08-02 21:08:40 -0500

    navigation responsive appbar styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import ResponsiveAppBar from '../Navigation';
 import Navigation from '../Navigation';
 
 
 function Header() {
     return (
         <div>
-            <Navigation></Navigation>
+            <ResponsiveAppBar></ResponsiveAppBar>
             <div>
                 <h1>A Dish A Day</h1>
                 <p>Nav component coming soon</p>
