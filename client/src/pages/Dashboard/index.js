b81a86121df521b41d49e1c41c45dcc1521a8704
Emma Olson <emma.olson794@gmail.com>
2022-08-04 15:39:25 -0500

    basic structure for dashboard page

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import dashStyles from './dashboard.module.css';
 import SecondaryNav from '../../components/SecondaryNav';
+import Footer from '../../components/Footer';
 
 const Dashboard = () => {
 
+    const recipes = [
+        {
+            recipeTitle: "Chicken Parm",
+            description: "This is a great chicken dish",
+            author: "Emma",
+            img: "https://placekitten.com/400/200",
+            ingredient: ["chicken", "parmesan", "tomato sauce", "pasta"],
+            preprationStep: ["1: do this", "2: to this second", "3: do this third"],
+            tag: ["Chicken"]
+        }
+    ]
+
     return (
-        <div>
+        <div className={dashStyles.test}>
             <SecondaryNav></SecondaryNav>
-            <h2>Dashboard</h2>
+                <section>
+                    <h2>Dashboard</h2>
+
+                    {/* Recent recipes */}
+                    <section>
+                            
+                    </section>
+
+                    {/* Review section */}
+                    <section>
+
+                    </section>
+                </section>
+            <Footer />
         </div>
     )
 }
