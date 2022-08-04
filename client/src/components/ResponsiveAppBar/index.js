54739036a6370718e7a16c7c66f8aeb73c8504e5
Emma Olson <emma.olson794@gmail.com>
2022-08-04 11:39:12 -0500

    remove avatar icon from nav

--- client/src/components/ResponsiveAppBar/index.js
+++ client/src/components/ResponsiveAppBar/index.js
@@ -186,12 +186,7 @@ const ResponsiveAppBar = () => {
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Open settings">
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg"
-                                style={{
-                                    color: 'white',
-                                    backgroundColor: 'black',
-
-                                }} />
+                                <MenuIcon />
                             </IconButton>
                         </Tooltip>
                         <Menu
