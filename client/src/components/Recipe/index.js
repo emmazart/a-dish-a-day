2943e32f6d8df60114d211d56b9748589bb5f622
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-08-04 11:07:02 -0400

    disabled VS code extensions @built in typscript due to error

--- client/src/components/Recipe/index.js
+++ client/src/components/Recipe/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import * as React from 'react';
-// import { styled } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
@@ -18,20 +18,20 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Rating from '@mui/material/Rating';
 import Rating from '../Rating';
 
-// interface ExpandMoreProps extends IconButtonProps {
-//   expand: boolean;
-// }
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
 
-// const ExpandMore = styled((props: ExpandMoreProps) => {
-//   const { expand, ...other } = props;
-//   return <IconButton {...other} />;
-// })(({ theme, expand }) => ({
-//   transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-//   marginLeft: 'auto',
-//   transition: theme.transitions.create('transform', {
-//     duration: theme.transitions.duration.shortest,
-//   }),
-// }));
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 export default function RecipeCard({recipe}) {
   const [expanded, setExpanded] = React.useState(false);
