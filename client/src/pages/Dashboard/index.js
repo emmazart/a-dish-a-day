a1cd7a44a185322ebc21706ec19c725614df91d3
Emma Olson <emma.olson794@gmail.com>
2022-08-04 15:49:25 -0500

    basic setup for dashboard cont

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 import dashStyles from './dashboard.module.css';
 import SecondaryNav from '../../components/SecondaryNav';
+import Recipe from '../../components/Recipe';
 import Footer from '../../components/Footer';
 
 const Dashboard = () => {
 
     const recipes = [
+        {
+            recipeTitle: "Chicken Parm",
+            description: "This is a great chicken dish",
+            author: "Emma",
+            img: "https://placekitten.com/400/200",
+            ingredient: ["chicken", "parmesan", "tomato sauce", "pasta"],
+            preprationStep: ["1: do this", "2: to this second", "3: do this third"],
+            tag: ["Chicken"]
+        },
+        {
+            recipeTitle: "Chicken Parm",
+            description: "This is a great chicken dish",
+            author: "Emma",
+            img: "https://placekitten.com/400/200",
+            ingredient: ["chicken", "parmesan", "tomato sauce", "pasta"],
+            preprationStep: ["1: do this", "2: to this second", "3: do this third"],
+            tag: ["Chicken"]
+        },
         {
             recipeTitle: "Chicken Parm",
             description: "This is a great chicken dish",
@@ -20,19 +40,32 @@ const Dashboard = () => {
     return (
         <div className={dashStyles.test}>
             <SecondaryNav></SecondaryNav>
-                <section>
-                    <h2>Dashboard</h2>
+            <h2>Dashboard</h2>
+                <main className={dashStyles.main}>
 
-                    {/* Recent recipes */}
-                    <section>
-                            
-                    </section>
+                        {/* Recent recipes */}
+                        <section>
+                            {recipes.map((recipe, index) => {
+                                return (
+                                    <card key={index}>
+                                        <img src={recipe.img}></img>
+                                        <h2>{recipe.recipeTitle}</h2>
+                                        <p>{recipe.description}</p>
+                                    </card>
+                                )
+                            })}
+                                
+                        </section>
 
-                    {/* Review section */}
-                    <section>
-
-                    </section>
-                </section>
+                        {/* Review section */}
+                        <section>
+                            <form>
+                                <h2>Review this recipe</h2>
+                                <textarea></textarea>
+                                <Button>Submit</Button>
+                            </form>
+                        </section>
+                </main>
             <Footer />
         </div>
     )
