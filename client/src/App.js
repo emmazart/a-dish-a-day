f424e85995bbef5b8d4d9f13d51990769c56de90
MNlegion <rkreuser30@gmail.com>
2022-08-08 18:54:30 -0500

    profile and profile routing

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,12 @@ import Landing from '../src/pages/Landing';
 import SignIn from '../src/components/SignIn';
 import SignUp from '../src/components/SignUp';
 import About from './pages/About';
+import Profile from './pages/Profile';
 import NoMatch from '../src/pages/NoMatch';
 import Dashboard from './pages/Dashboard';
 import RecipeSearch from './pages/AllRecipes';
 import SingleRecipe from './pages/SingleRecipe';
+import { Typography } from '@mui/material';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql',
@@ -58,6 +60,10 @@ function App() {
             path="/recipes/*"
             element={<SingleRecipe />}
           />
+          <Route
+            path="/profile"
+            element={<Profile />}
+            />
           <Route 
             path="*"
             element={<NoMatch />}
--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function Profile() {
     return (
@@ -14,6 +17,39 @@ function Profile() {
             >
                 My Profile
             </Typography>
+
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+    <div>
+    <TextField
+          id="filled-helperText"
+          label=""
+          defaultValue=""
+          helperText="User Name"
+          variant="filled"
+        />
+        <TextField
+          id="filled-helperText"
+          label=""
+          defaultValue="Default Value"
+          helperText="Some important text"
+          variant="filled"
+        />
+        <TextField
+          id="filled-helperText"
+          label=""
+          defaultValue="Default Value"
+          helperText="Some important text"
+          variant="filled"
+        />
+      </div>
+    </Box>
         <Footer></Footer>
         </>
     )
