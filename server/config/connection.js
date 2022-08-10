bc03acee8f8381c293e35e6fbe0288f8902e2ed6
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-08-10 13:52:39 -0400

    Case for db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:38797/dish', { //reminder to change name
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:38797/Dish', { //reminder to change name
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
