77b9d4936e249a7baa4e25ff0313be914ff13b6b
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-08-10 13:48:48 -0400

    Updating port

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/dish', { //reminder to change name
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:38797/dish', { //reminder to change name
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
